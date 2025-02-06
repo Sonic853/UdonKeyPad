d62ab5891829f254b5d1da553b47ef600779300f
Sonic853 <Sonic853@qq.com>
2025-02-06 15:53:57 +0800

    vr use canvas btn at keypad model ver

---
+++ Scripts/UdonVRCheckerObjects.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Sonic853.Udon.Keypad
+{
+    public class UdonVRCheckerObjects : UdonSharpBehaviour
+    {
+        public GameObject[] pcObjects;
+        public GameObject[] vrObjects;
+        void Start()
+        {
+            var isVr = Networking.LocalPlayer.IsUserInVR();
+            foreach (var obj in pcObjects)
+            {
+                obj.SetActive(!isVr);
+            }
+            foreach (var obj in vrObjects)
+            {
+                obj.SetActive(isVr);
+            }
+        }
+    }
+}
